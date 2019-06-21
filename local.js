7098e12a416c63f8db8956eca962f23293057742
Alden <rivendalejones@gmail.com>
2019-06-20 21:02:04 -0400

    adding custom marquee

--- local.js
+++ local.js
@@ -56,7 +56,7 @@ var duration = moment.duration(between_now_and_event).minutes();
 const marquee_message =
   `<li id="xkcd"><marquee>
 
-  1.5˚ Climate Crisis ${years_to_event} years <span id="minutes" class='part'>${duration}</span>min
+  925 days until federal student debt hits $2 trillion
 
   </marquee></li>`;
 
